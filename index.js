bce30cc3880ad714b1b569fa1f23932b67ff5c2e
p-karthik-eng <karthikputcha123@gmail.com>
2025-10-18 09:51:23 +0530

    change file name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import transcriptRoute from "./routes/transcriptRoute.js";
+import transcriptRoute from "./routes/getTranscriptRoute.js";
 import proxyRoute from "./routes/proxyRoute.js";
 
 dotenv.config();
